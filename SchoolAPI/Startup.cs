9d8bc13ce8fe805f24016a500bc83d94a97f1e6f
nourahedhli <nh98@njit.edu>
2021-04-16 05:48:41 -0400

    module 5

--- SchoolAPI/Startup.cs
+++ SchoolAPI/Startup.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
@@ -10,13 +11,16 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using NLog;
 
 namespace SchoolAPI
 {
     public class Startup
     {
         public Startup(IConfiguration configuration)
+
         {
+            LogManager.LoadConfiguration(string.Concat(Directory.GetCurrentDirectory(), "/nlog.confg"));
             Configuration = configuration;
         }
 
