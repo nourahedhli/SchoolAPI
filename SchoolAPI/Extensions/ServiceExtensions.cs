29deaadd041402ce0d29e361dcf700ac49626338
nourahedhli <nh98@njit.edu>
2021-04-16 05:48:33 -0400

    module 5

---
+++ SchoolAPI/Extensions/ServiceExtensions.cs
@@ -0,0 +1,16 @@
+﻿using Contracts;
+using LoggerManager;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace SchoolAPI.Extensions
+{
+    public static class ServiceExtensions
+    {
+
+        public static void ConfigureLoggerService(this IServiceCollection services)
+        {
+            services.AddScoped<ILoggerManager, LoggerService>();
+        }
+    }
+}
